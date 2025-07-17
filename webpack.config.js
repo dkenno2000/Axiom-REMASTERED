31309d2f1cabc6788c728392a8ce4981f254754c
dkenno2000 <d.kenno2000@gmail.com>
2025-07-17 03:09:14 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production", // Change to "development" if needed
+  // devtool: "source-map",
+  devtool: false,
+  entry: "./src/remastered.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "remastered.bundle.js"
+  },
+  target: "web", // Ensure it's targeting web for a browser environment
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /lightweight-charts\.standalone\.production\.js$/,
+        loader: 'exports-loader',
+        options: {
+          exports: {
+            LightweightCharts: 'LightweightCharts',
+            createChart: ['LightweightCharts', 'createChart']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'lightweight-charts': path.resolve(__dirname, 'node_modules/lightweight-charts/dist/lightweight-charts.standalone.production.js')
+    }
+  }
+};
+
